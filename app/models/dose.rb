af65b5394c833afdfbc47e8a336384a204880d6a
Amath <amathmagnan@gmail.com>
2021-02-20 15:32:04 +0100

    cocktail ingredients doses rake ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
-  belongs_to :ingredient
+  belongs_to :ingredient, dependent: :destroy
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
