f72787f16bc43d654d85551fba4fab0bd339c211
Amath <amathmagnan@gmail.com>
2021-02-22 17:06:18 +0100

    controller to fix

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,21 @@
 class DosesController < ApplicationController
+
+
+
+def index
+  @dose = Dose.all
+end
+
+def new
+  @dose = Dose.all
+end
+
+def show
+  @dose = Doses.all
+end
+
+def create
+end
+
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+
+
+
+def index
+  @cocktails = Cocktail.all
+end
+
+def new
+end
+
+def show
+  @cocktails = Cocktail.all
+end
+
+def create
+end
+
+
+
 end
